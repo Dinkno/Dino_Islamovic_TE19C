04b5e4db1f5ad68a259bd0c1a589a772e3650d3a
Zsofia Nemethy <70264591+Dinkno@users.noreply.github.com>
2020-08-31 14:23:33 +0200

    Första lektion

--- variabler/var.py
+++ variabler/var.py
@@ -1,1 +1,14 @@
-print("hej")
+print("hej TE19C")
+
+# skapar en variabel och tilldelar det värdet "Dino"
+name = "Dino" 
+
+#print("Ditt namn är",name)
+#print(name)
+print(f"Ditt namn är {name}")
+
+# float typomvandlar strängen till ett flytal för att räkna med den
+side = float(input("Ange kvadratens sida: ")) 
+area = side**2
+
+print(f"Arean av kvadraten är {area} a.e.")
