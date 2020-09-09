e5f999dacb0c6a7a482dc2665c2cc8d04ca829fb
Zsofia Nemethy <70264591+Dinkno@users.noreply.github.com>
2020-09-09 13:47:41 +0200

    commitar massor av ändringar

--- variabler/var.py
+++ variabler/var.py
@@ -10,5 +10,6 @@ print(f"Ditt namn är {name}")
 # float typomvandlar strängen till ett flytal för att räkna med den
 side = float(input("Ange kvadratens sida: ")) 
 area = side**2
-
+omkrets = 4*side
 print(f"Arean av kvadraten är {area} a.e.")
+#print(f"omkretsen är {omkrets}l.e och area är {area}a.e")
